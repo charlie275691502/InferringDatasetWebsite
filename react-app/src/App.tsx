36e529e681e603607a918f43d2d25d92d44aa3d8
charlie275691502 <charlie275691502@gmail.com>
2024-03-14 09:22:45 +1100

    add basic axois request

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import Button from "./components/Button";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const request = () => {
+    axios
+      .get("http://127.0.0.1:9000/dataset_handler/datasets/preview/15")
+      .then((response) => {
+        console.log(JSON.parse(response.data.data));
+      })
+      .catch((error) => {
+        console.log("Error:", error);
+      });
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <Button text="Primary" onClickButton={request} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
