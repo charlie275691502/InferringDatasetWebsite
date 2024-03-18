28f92a09c090e77d2bdbfb9c2757e1aa5c98d317
charlie275691502 <charlie275691502@gmail.com>
2024-03-18 12:36:28 +1100

    Add api auto generate library

--- Django/main/urls.py
+++ Django/main/urls.py
@@ -18,8 +18,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='API')
 
 urlpatterns = [
+    path('api/', schema_view),
     path('admin/', admin.site.urls),
     path('dataset_handler/', include('dataset_handler.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
