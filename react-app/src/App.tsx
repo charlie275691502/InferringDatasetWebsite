becfe95f5a82924d10a20ceaea652da1757c1f0b
charlie275691502 <charlie275691502@gmail.com>
2024-03-20 11:27:34 +1100

    modify tabs

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,65 +1,19 @@
-import Button from "./components/Button";
-import DatasetTable from "./components/DatasetTable";
-import { Data } from "./components/DatasetTable";
-import { useReducer, useState } from "react";
-import FileUpload from "./components/FileUpload";
-import {
-  FileDownload,
-  GetRequest,
-  PostRequest,
-  PutRequest,
-} from "./modules/Request";
+import Tabs from "./components/Tabs";
+import RawDataSubPage from "./components/RawDataSubPage";
+import SummarySubPage from "./components/SummarySubPage";
+import { useState } from "react";
 
 function App() {
-  const domain = "http://127.0.0.1:9000";
-  let [data, setData] = useState<Data | null>(null);
-  const [, forceUpdate] = useReducer((x) => x + 1, 0);
-
-  const getPreviewDatasets = (dataset_id: number) =>
-    GetRequest(
-      `${domain}/dataset_handler/datasets/${dataset_id}/`,
-      (response) => setData(response)
-    );
-
-  const uploadFile = (file: File) =>
-    PostRequest(
-      `${domain}/dataset_handler/datasets/upload/`,
-      { raw_file: file },
-      (response) => setData(response)
-    );
-
-  const downloadFile = (dataset_id: number) =>
-    GetRequest(
-      `${domain}/dataset_handler/datasets/download/${dataset_id}/`,
-      (response) => FileDownload(response.raw_file, response.file_name)
-    );
-
-  const selectDropdownElement = (
-    dataset_id: number,
-    column_id: number,
-    element: string
-  ) =>
-    PutRequest(
-      `${domain}/dataset_handler/datasets/${dataset_id}/columns/${column_id}/`,
-      { type: element },
-      (response) => {
-        let column = data?.columns?.find((column) => column.id == response.id);
-        if (column) column.type = response.type;
-        forceUpdate();
-      }
-    );
+  const pageNames = ["Raw Data", "Summary"];
+  let [pageName, setPageName] = useState<string>();
+  let onSelectPage = (index: number) => {
+    setPageName(pageNames[index]);
+  };
 
   return (
     <div>
-      <FileUpload onUpload={uploadFile} />
-      <Button text="Download" onClickButton={() => downloadFile(14)} />
-      <Button text="Cheating" onClickButton={() => getPreviewDatasets(14)} />
-      {data && (
-        <DatasetTable
-          data={data}
-          onDropdownElementSelect={selectDropdownElement}
-        />
-      )}
+      <Tabs pageNames={pageNames} onSelectPage={onSelectPage} />
+      {pageName == pageNames[0] ? <RawDataSubPage /> : <SummarySubPage />}
     </div>
   );
 }
